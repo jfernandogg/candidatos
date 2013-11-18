18e1ed734ef57b12968ea7244c143496102daa35
unknown <fernando.gallego@ESTACION157.ceiba.local>
2013-11-18 16:17:12 -0500

    Initial commit. Este es el proyecto inicial

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\fernando.gallego\Desktop\my-app\bin\AndroidManifest.xml : \
+C:\Users\fernando.gallego\Desktop\my-app\AndroidManifest.xml \
---
+++ bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes.dex : \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\BuildConfig.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\CordovaExample.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\R$attr.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\R$drawable.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\R$string.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\R$xml.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\classes\my\cordova\project\R.class \
+C:\Users\fernando.gallego\Desktop\my-app\bin\dexedLibs\cordova-3.1.0-1614da78bc78596822086f0ee8936783.jar \
